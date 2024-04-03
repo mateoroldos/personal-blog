c55c9381101d207b29d652d3a0fa765b57730df3
mateoroldos <mateoroldos19@gmail.com>
2024-04-03 17:55:12 -0300

    fix: Update projects and general info

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,4 +1,4 @@
-import { z } from 'astro:content';
+import { z } from "astro:content";
 
 const projectSchema = z.object({
   repo: z.string(),
@@ -11,34 +11,34 @@ export type Project = z.infer<typeof projectSchema>;
 
 const projects: Project[] = [
   {
-    repo: 'project-toucans',
-    link: 'https://github.com/emerald-dao/project-toucans-v2',
-    description: 'Create and manage DAOs on the Flow Blockchain.',
-    tags: ['Svelte', 'TypeScript', 'Flow'],
+    repo: "project-toucans",
+    link: "https://github.com/emerald-dao/project-toucans-v2",
+    description: "Create and manage DAOs on the Flow Blockchain.",
+    tags: ["Svelte", "TypeScript", "Flow"],
   },
   {
-    repo: 'tokenbase',
-    link: 'https://github.com/mateoroldos/tokenbase',
-    description: 'Create, manage and export design tokens.',
-    tags: ['Svelte', 'TypeScript', 'Tailwind CSS'],
+    repo: "tokenbase",
+    link: "https://github.com/mateoroldos/tokenbase",
+    description: "Create, manage and export design tokens.",
+    tags: ["Svelte", "TypeScript", "Tailwind CSS"],
   },
   {
-    repo: 'floats',
-    link: 'https://github.com/emerald-dao/float',
-    description: 'Flow proof of attendance dapp.',
-    tags: ['Svelte', 'TypeScript', 'Flow'],
+    repo: "floats",
+    link: "https://github.com/emerald-dao/float",
+    description: "Flow proof of attendance dapp.",
+    tags: ["Svelte", "TypeScript", "Flow"],
   },
   {
-    repo: 'personal-blog',
-    link: 'https://github.com/mateoroldos/obsidian-blog',
-    description: 'Astro template to create a blog from Obsidian notes.',
-    tags: ['Astro', 'React', 'TypeScript', 'Tailwind'],
+    repo: "personal-blog",
+    link: "https://github.com/mateoroldos/personal-blog",
+    description: "Cool personal site.",
+    tags: ["Astro", "Svelte", "TypeScript", "Tailwind"],
   },
   {
-    repo: 'emerald-academy',
-    link: 'https://github.com/emerald-dao/emerald-academy-v2',
-    description: 'A platform for learning about the Flow Blockchain ecosystem.',
-    tags: ['Svelte', 'TypeScript', 'Flow'],
+    repo: "emerald-academy",
+    link: "https://github.com/emerald-dao/emerald-academy-v2",
+    description: "A platform for learning about the Flow Blockchain ecosystem.",
+    tags: ["Svelte", "TypeScript", "Flow"],
   },
 ];
 
