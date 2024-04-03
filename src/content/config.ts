4d8a96d87c3c6b0a6b36e4feed93c6e57fd536ce
mateoroldos <mateoroldos19@gmail.com>
2024-04-03 18:01:47 -0300

    fix: Make tags optional in blog posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     isDraft: z.boolean().optional().default(false),
     title: z.string(),
     description: z.string(),
-    date: z.string().transform((str) => new Date(str)),
-    tags: z.array(z.string()),
+    date: z.string().transform(str => new Date(str)),
+    tags: z.array(z.string()).optional(),
   }),
 });
 
