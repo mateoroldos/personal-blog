0b7b4d91a691dae3f15c6ccf098fb164ee763b3a
mateoroldos <mateoroldos19@gmail.com>
2024-04-03 18:52:56 -0300

    feat: API route improvement

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
 import { z } from "zod";
 
+export const prerender = false;
+
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
   const email = data.get("email");
@@ -25,28 +27,28 @@ export const POST: APIRoute = async ({ request }) => {
 
   const RESEND_API_KEY = import.meta.env.RESEND_API_KEY;
 
-  // const response = await fetch("https://api.resend.com/emails", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     Authorization: `Bearer ${RESEND_API_KEY}`,
-  //   },
-  //   body: JSON.stringify({
-  //     from: "onboarding@resend.dev",
-  //     to: "mateo@emestudio.xyz",
-  //     subject: "Message from personal site",
-  //     html: `${email}: ${message}`,
-  //   }),
-  // });
+  const response = await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
+      from: "onboarding@resend.dev",
+      to: "mateo@emestudio.xyz",
+      subject: "Message from personal site",
+      html: `${email}: ${message}`,
+    }),
+  });
 
-  // if (!response.ok) {
-  //   return new Response(
-  //     JSON.stringify({
-  //       message: "Failed to send email",
-  //     }),
-  //     { status: 500 }
-  //   );
-  // }
+  if (!response.ok) {
+    return new Response(
+      JSON.stringify({
+        message: "Failed to send email",
+      }),
+      { status: 500 }
+    );
+  }
 
   return new Response(
     JSON.stringify({
